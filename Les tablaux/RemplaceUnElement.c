402acc71d2db49efa286724d7040b017c2fe1457
Houssam Bensiyed <you@example.com>
2025-09-03 12:06:11 +0100

    add RemplaceUnELement

--- Les tablaux/RemplaceUnElement.c
+++ Les tablaux/RemplaceUnElement.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    int i, n, 
+    int i, elements, oldElement, newElement;
+
+    bool Success = false;
+
+    printf("Vueillez saisir les nombres des elements: ");
+    scanf("%d", &elements);
+    int tableau[elements];
+    
+    for (i = 0; i < elements; i++) {
+        printf("Saisir l'element %d: ", i + 1);
+        scanf("%d", &tableau[i]);
+    }
+    
+    printf("Vueillez saisir la valeur a remplacer: ");
+    scanf("%d", &oldElement);
+    printf("Vueillez saisir la nouvelle valeur: ");
+    scanf("%d", &newElement);
+
+    for (i = 0; i < elements; i++){
+        if (tableau[i] == oldElement) {
+            tableau[i] = newElement;
+            Success = true;
+        }
+    }
+    if (Success) {
+        printf("L'operation a été avec succèes!\n");
+        printf("Voice la nouvelle version du tableau: ");
+        
+        for( i = 0; i < elements; i++) {
+            printf("{ ");
+            if (i == elements - 1) {
+                printf("%d}", tableau[i]);
+            } else {
+            printf("%d,", tableau[i]);
+            }
+        }
+    }
 }
